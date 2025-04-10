7e72a43e90456a6560c1a89058572ea1f05ee92b
Omax64MXG4ming <tuy54190@gmail.com>
2025-04-10 08:19:16 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include("customEmbed1.php"); ?>
+
+
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+
+
+<body style="background-color:grey">
+  
+</body>
+
+
+<style>
+
+	.gdButtonBrowser {
+
+		cursor: pointer;
+
+	}
+
+</style>
+
+<body class="levelBG" onbeforeunload="saveUrl()">
+
+<div id="everything">
+
+ <div class="popup" id="credits"> </div> <div style="position:absolute; bottom: -50%; left: 0%; width: 100%; pointer-events: none"> <img class="cornerPiece" src="browser/assets/corner.png" width=7%;> </div> <div style="position:absolute; bottom: -50%; right: 0%; width: 100%; text-align: right;"> <img class="cornerPiece" src="browser/assets/corner.png" width=7%; style="transform: scaleX(-1)"> </div>
+
+  <div class="browserIcon" style="position:absolute; top: 36.3%; left: 50%; text-align: left; width: 10%;"> <a href="/browser/"><img class="browserIcon" src="browser.png" width="70%"></a> </div>
+
+
+            <div class="supercenter center" id="menubuttons" style="bottom: 5%;"> <table> <tr class="menuButtonList">
+
+
+<div class="dashboardIcon" style="position:absolute; top: 36.5%; left: 42%; text-align: left; width: 10%;"> <a href="/dashboard/"><img class="dashboardIcon" src="dashboard.png" width="70%"></a> </div>
+
+
+<div class="discordIcon" style="position:absolute; top: 57%; left: 55%; text-align: left; width: 10%;"> <a href="https://discord.gg/7S3fwnuWeE"><img class="discordIcon" src="discord.png" width="70%"></a> </div>
+
+<div class="stargdpsIcon" style="position:absolute; top: 10%; left: 30%; text-align: left; width: 10%;"> <img class="stargdpsIcon" src="stargalaxygdps.png" width="400%"> </div> 
+
+<div class="downloadType" style="position:absolute; top: 57%; left: 40%; text-align: left; width: 10%;"> <a href="https://www.mediafire.com/file/5bzpnjw68e54hgi/ST%F0%9F%92%ABR_GALAXY_GDPS_Invisible_Edition.apk/file"><img class="downloadType" src="android.png" width="70%"></a> </div>
+
+<div class="typeDownload" style="position:absolute; top: 57%; left: 47.5%; text-align: left; width: 10%;"> <a href="/"><img class="typeDownload" src="windows.png" width="70%"></a> </div>
+
+
+<div class="omaxType" style="position:absolute; top: 110%; left: 35%; text-align: left; width: 10%;"> <img class="omaxType" src="omax.png" width="300%"> </div>
+
+
+<div class="dixonType" style="position:absolute; top: 128%; left: 35%; text-align: left; width: 10%;"> <img class="dixonType" src="dixon.png" width="270%"> </div>
+
+<div class="freeType" style="position:absolute; top: 60%; left: 0.5%; text-align: left; width: 10%;"> <a href="/dashboard/login/register.php"><img class="freeType" src="free.png" width="80%"></a> </div>
+
+<div class="songType" style="position:absolute; top: 57%; left: 32.7%; text-align: left; width: 10%;"> <a href="/browser/songs/?"><img class="songType" src="song.png" width="70%"></a> </div>
+
+
+<div class="gdps" style="position:absolute; top: 57%; left: 25.5%; text-align: left; width: 10%;"> <a href="https://gdpshub.com/gdps/2923"><img class="gdps" src="gdpshub.png" width="70%"></a> </div>
+
+
+<div class="search" style="position:absolute; top: 36.5%; left: 33.5%; text-align: left; width: 10%;"> <a href="/browser/search/"><img class="search" src="search.png" width="70%"></a> </div>
+
+<div class="iosType" style="position:absolute; top: 36%; left: 58%; text-align: left; width: 10%;"> <a href="https://www.mediafire.com/file/aao069eswbhacw3/St%25F0%259F%2592%25ABrGalaxyGDPS.ipa/file"><img class="iosType" src="iOS.png" width="70%"></a> </div>
+
+
+<!-- <div class="" style="position:absolute; top: 45%; left: 0%; text-align: left; width: 10%;"> <a href="/"><img class="" src=".png" width="70%"></a> </div>  -->
+
+
+<!--   <a href="https://www.youtube.com/@omax64mxg4ming"</a> -->
